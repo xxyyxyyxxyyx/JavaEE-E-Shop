f57742cb923486d9665e9c46696e9de9af1bbdb4
Sunil Limbu <xxyyxyyxxyyx@gmail.com>
2017-05-18 11:52:32 +0800

    Commit

---
+++ src/BookServlet.java
@@ -0,0 +1,56 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BookServlet
+ */
+@WebServlet("/BookServlet")
+public class BookServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BookServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		Connection conn = null;
+		Statement stmt = null;
+		try{
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ebookshop","root","xxyyxyyx");
+			stmt = conn.createStatement();
+			String author = request.getParameter("author");
+			String select = "Select * from books where author='"+author+"'";
+			ResultSet rset = stmt.executeQuery(select);
+			out.println("Query Result");
+		}
+		catch(SQLException ex){
+			ex.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
